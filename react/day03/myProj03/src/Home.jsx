ee73c12b763830352f5e1dfe372265a205bef4ea
keshavtiwari001 <keshut001@gmail.com>
2024-04-26 20:37:54 +0530

    added

---
+++ react/day03/myProj03/src/Home.jsx
@@ -0,0 +1,11 @@
+function Home(){
+    return(
+        <div className="home">
+            <div className="hometxt">
+                <h4>BEST OFFER</h4>
+                <h1>NEW ARRIVALS ON SALE</h1>
+            </div>
+        </div>
+    )
+}
+export default Home;
---
+++ react/day03/myProj03/src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+import './App.css'
+import Header from './Header.jsx'
+import Home from './Home.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <Header />
+    <Home />
+  </React.StrictMode>,
+)
