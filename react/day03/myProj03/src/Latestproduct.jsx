6cbb70d87ab802f07d42cf7fcffe44ee759dab7c
keshavtiwari001 <keshut001@gmail.com>
2024-04-27 20:55:16 +0530

    added

---
+++ react/day03/myProj03/src/Latestproduct.jsx
@@ -0,0 +1,10 @@
+import ProductCard from './ProductCard'
+function Latestproduct(){
+    return(
+        <div className="latestproduct">
+            <h2 className="headingH2">Latest Products</h2>
+            <ProductCard />
+        </div>
+    )
+}
+export default Latestproduct; 
--- react/day03/myProj03/src/main.jsx
+++ react/day03/myProj03/src/main.jsx
@@ -4,11 +4,15 @@ import ReactDOM from 'react-dom/client'
 import './App.css'
 import Header from './Header.jsx'
 import Home from './Home.jsx'
+import Latestproduct from './Latestproduct.jsx'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
     <Header />
     <Home />
-  </React.StrictMode>,
+    <Latestproduct />
+  </React.StrictMode>
 )
