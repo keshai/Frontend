a366d014ecbe512876e0b0dbb78d759f83b20740
keshavtiwari001 <keshavtiwari.dev@gmail.com>
2024-07-02 13:02:08 +0530

    updated

---
+++ javaScript/test/june_29/test.js
@@ -0,0 +1,7 @@
+// 1. calculate the factorial of 5 using reduce method
+// const numbers = [1,2,3,4,5];
+// const sum = numbers.reduce((acc,current)=> acc + current,0);
+// console.log(sum);
+
+// 2. find the maximum of n numbers passed from function using reduce
+function maxNum= 
